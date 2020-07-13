5e6b9e6b36fefefcccd23816679e37ec703832cf
sargaskim <2103702@talnet.nl>
2020-07-13 16:10:23 +0200

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,3 +1,16 @@
- CREATE DATABASE sterrenstelsel;
+CREATE DATABASE sterrenstelsel;
 USE sterrenstelsel;
 CREATE TABLE planeten (name varchar(10));
+
+INSERT INTO planeten (name)
+VALUES ('Zon');
+INSERT INTO planeten (name)
+VALUES ('Mercurius');
+INSERT INTO planeten (name)
+VALUES ('Venus');
+INSERT INTO planeten (name)
+VALUES ('Aarde');
+INSERT INTO planeten (name)
+VALUES ('Mars');
+
+SELECT * FROM planeten;
